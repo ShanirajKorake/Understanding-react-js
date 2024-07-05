b641f77f4e3caf2fa6bdc15621ef1d464f3e249b
om <korkeom11@gmail.com>
2024-07-05 11:56:13 +0530

    React.creactElement()

--- 01_vite-react/src/main.jsx
+++ 01_vite-react/src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+const recEl = React.createElement(
+  'a',
+  {href: 'https://google.com',target: '_blank' },
+  'click me to visit google',
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    recEl
 )
