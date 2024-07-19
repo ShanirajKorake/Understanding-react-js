93126b41c071ca01c2c88ce5ff215b6e29a7d97b
om <korkeom11@gmail.com>
2024-07-19 14:07:53 +0530

    Mega Project : some components

--- 12-mega-blog/src/components/index.js
+++ 12-mega-blog/src/components/index.js
@@ -1,7 +1,12 @@
+import Container from "./container/Container";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
-
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
 }
