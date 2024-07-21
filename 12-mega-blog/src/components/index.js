3b187b599950a2fde9ee72a97b2384d7226b3dc7
om <korkeom11@gmail.com>
2024-07-21 15:01:12 +0530

    Mega Project : Hooks done

--- 12-mega-blog/src/components/index.js
+++ 12-mega-blog/src/components/index.js
@@ -2,11 +2,17 @@ import Container from "./container/Container";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
 import Logo from "./Logo";
+import Button from "./Button";
+import Select from "./Select";
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Input,
+    Select,
+    Button
 }
